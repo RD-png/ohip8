fc7a661d95993588f8e3174f2bfd2143a5baf6ae
Ryan Denby <ryan.543@live.co.uk>
2023-10-19 21:22:33 +0100

    Add ohip8.mli

---
+++ lib/ohip8.mli
@@ -0,0 +1,7 @@
+type chip_context = { mutable memory : Memory.t;
+                      mutable display : Display.t }
+
+val create_chip_context : chip_context
+
+val pp_chip_context : Format.formatter -> chip_context -> unit
+val show_chip_context : chip_context -> string
