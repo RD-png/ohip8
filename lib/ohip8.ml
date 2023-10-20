76daf3e647c7ddac8cd8f02de97b2ff9650d073b
Ryan Denby <ryan.543@live.co.uk>
2023-10-20 23:58:43 +0100

    Implement decode logic

--- lib/ohip8.ml
+++ lib/ohip8.ml
@@ -6,6 +6,8 @@ type chip_context =
     sound_timer     : int
   }
 
+exception Invalid_opcode of string;;
+
 let create_chip_context =
   { memory      = Memory.create;
     display     = Display.init;
@@ -14,3 +16,20 @@ let create_chip_context =
     sound_timer = 0
   }
 ;;
+
+(* Maybe move this logic to a decoder module *)
+
+let nibble byte shift bound =
+  Int.shift_left (Int.logand byte bound) shift
+;;
+
+let decode opcode =
+  let optype = nibble opcode 4 0xF0 in
+  match optype with
+  | 0 -> () (* 00E0 - Clear Screen *)
+  | 1 -> () (* 1NNN - Jump *)
+  | 2 -> () (* 6XNN - Set Register VX *)
+  | 3 -> () (* 7XNN - Add Value To Register VX *)
+  | 4 -> () (* ANNN - Set Index Register I *)
+  | 5 -> () (* DXYN - Display / Draw *)
+  | _ -> raise (Invalid_opcode (Int.to_string optype))
