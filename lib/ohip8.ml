926b0b6880ccc4b287527571a2d48fa4581055c2
Ryan Denby <ryan.543@live.co.uk>
2023-10-20 23:57:52 +0100

    Add stack, delay_timer, sound_timer to chip_context

--- lib/ohip8.ml
+++ lib/ohip8.ml
@@ -1,10 +1,16 @@
 type chip_context =
   { mutable memory  : Memory.t;
-    mutable display : Display.t
-  } [@@deriving show]
+    mutable display : Display.t;
+    stack           : int Stack.t;
+    delay_timer     : int;
+    sound_timer     : int
+  }
 
 let create_chip_context =
-  { memory  = Memory.create;
-    display = Display.init
+  { memory      = Memory.create;
+    display     = Display.init;
+    stack       = Stack.create ();
+    delay_timer = 0;
+    sound_timer = 0
   }
 ;;
