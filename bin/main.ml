f75abfc54d958c0cdf36fea523851de2852770a1
Ryan Denby <ryan.543@live.co.uk>
2023-10-21 00:00:42 +0100

    Begin to implement fetch/decode/eval loop

--- bin/main.ml
+++ bin/main.ml
@@ -1,5 +1,22 @@
 open Ohip8
 
+(* We will set a default speed of 700 instructions per second, however it
+   is configurable from the command line. *)
+
+let execute _action frequency =
+  if frequency > 0
+  then 1
+  else 2
+;;
+
+let rec run context frequency =
+  let opcode = Memory.fetch context.memory in
+  let action = decode opcode in
+  let frequency' = execute action frequency in
+  run context frequency'
+;;
+
 let () =
   let context = create_chip_context in
-  show_chip_context context |> print_endline
+  run context 700
+;;
